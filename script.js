c316613fcb10ba3d6619ad00af6500921b99d162
Ganvs <gustavo.anverze@gmail.com>
2020-03-24 19:22:18 -0300

    other validations

--- script.js
+++ script.js
@@ -28,4 +28,22 @@ form.addEventListener('submit', function(e) {
   } else {
     showSuccess(username);
   }
+
+  if (email.value === '') {
+    showError(email, 'E-mail is required');
+  } else {
+    showSuccess(email);
+  }
+
+  if (password.value === '') {
+    showError(password, 'Password is required');
+  } else {
+    showSuccess(password);
+  }
+
+  if (password2.value === '') {
+    showError(password2, 'Confirm password is required');
+  } else {
+    showSuccess(password2);
+  }
 });
