8765accd57b1b749a1567f0c567e21d378de5730
Ganvs <gustavo.anverze@gmail.com>
2020-03-24 22:54:30 -0300

    passwords validation

--- script.js
+++ script.js
@@ -20,13 +20,18 @@ function showSuccess(input) {
 }
 
 // Check email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+
+  if (re.test(String(input.value.trim()).toLowerCase())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
 }
 
 // Check required fields
-function checkRquired(inputArr) {
+function checkRequired(inputArr) {
   inputArr.forEach(function(input) {
     if (input.value.trim() === '') {
       showError(input, `${getFieldName(input)} is required`);
@@ -53,6 +58,13 @@ function checkLenght(input, min, max) {
   }
 }
 
+// Check passwords match
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match');
+  }
+}
+
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
@@ -61,7 +73,9 @@ function getFieldName(input) {
 form.addEventListener('submit', function(e) {
   e.preventDefault();
 
-  checkRquired([username, email, password, password2]);
+  checkRequired([username, email, password, password2]);
   checkLenght(username, 3, 20);
   checkLenght(password, 6, 20);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
 });
